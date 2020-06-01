ccb0c6b03ec881d7da99893e514083f1437330e1
Tanya Hodges <tanyac52003@gmail.com>
2020-06-01 22:00:21 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,13 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
-end
+ winner = ""
+ passengers.each do |suite, name|
+  if suite == :suite_a && 
+ name.start_with?("A")
+    winner = name
+  end
+end 
+winner 
+end 
+
